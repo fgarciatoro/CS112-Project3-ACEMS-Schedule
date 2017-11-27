77a2881618251f92cebd7389873dcc3968aa05f6
whiteandnerdy42 <33734328+whiteandnerdy42@users.noreply.github.com>
2017-11-27 12:50:10 -0500

    Update Member.java

--- Member.java
+++ Member.java
@@ -1,10 +1,16 @@
 import java.util.ArrayList;
 Public Class Member {
   int rank;
-  ArrayList<Integer> availability;
+  ArrayList<Integer> availability; // 0 means not available, 1 is if needed, 2 is available
+  int numShifts;
   
   public Member(int rank,ArrayList<Integer> availability) {
     rank = this.rank;
     availability = this.availability;
+    numShifts=0;
+    Shift.allMembers+=this();
     }
+  public void plusShifts(){
+    numShifts++;
+  }
 }  
