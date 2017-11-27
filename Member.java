fba9afbc79e1f0dc4cd49bdfa39931aaec1b3a93
whiteandnerdy42 <33734328+whiteandnerdy42@users.noreply.github.com>
2017-11-27 12:12:44 -0500

    Create Member.java

---
+++ Member.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+Public Class Member {
+  int rank;
+  ArrayList<Integer> availability;
+  
+  public Member(int rank,ArrayList<Integer> availability) {
+    rank = this.rank;
+    availability = this.availibility;
+    }
+  
