c2823e717b000dca5a779ada903123b8138d22ef
whiteandnerdy42 <33734328+whiteandnerdy42@users.noreply.github.com>
2017-12-03 15:44:26 -0500

    Update ToRun.java

--- ToRun.java
+++ ToRun.java
@@ -2,3 +2,8 @@ import java.util.ArrayList;
 
 public class ToRun {
 ArrayList<Shift> shifts;
+public static void main(String args[]) {
+  Shift newShift;
+  for(int i = 0; i < Shift.allmembers.get(0).availability.length(); i++) {
+    newShift = new Shift(i);
+    shifts.add(newShift);
