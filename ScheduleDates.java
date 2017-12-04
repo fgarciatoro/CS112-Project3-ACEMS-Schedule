2640df608b376b14610cf012e8655d6e85089b1f
jgreenfield20 <33734332+jgreenfield20@users.noreply.github.com>
2017-12-04 15:19:49 -0500

    Jason changed the name of the variables

--- ScheduleDates.java
+++ ScheduleDates.java
@@ -21,26 +21,26 @@ public class ScheduleDates
     public static void ScheduleStart(){
         Scanner keyboard = new Scanner(System.in); 
         
-        String DoW;
-        int day;
-        int month;
-        int year;
+        String startDoW;
+        int startD;
+        int startM;
+        int startY;
         
-        System.out.println("On what day of the week does the schedule begin? (Ex: Monday) ");
-        DoW = keyboard.nextLine();
+        System.out.println("On what day of the week does the schedule start? (Ex: Monday) ");
+        startDoW = keyboard.nextLine();
         System.out.println();
         
-        System.out.println("On which day of the month does this day fall? (Ex: 1 for Monday, Dec. 1st) ");
-        day = keyboard.nextInt();    
+        System.out.println("On what day of the month does the schedule start? (Ex: 1 for Monday, Dec. 1st) ");
+        startD = keyboard.nextInt();    
         System.out.println();
         
-        System.out.println("On which month does the schedule begin? ");
+        System.out.println("During what month does the schedule begin? ");
         System.out.println("Enter the month's number. (Ex: For January, enter '1' ");
-        month = keyboard.nextInt();    
+        startM = keyboard.nextInt();    
         System.out.println();
         
-        System.out.println("What year is it?");
-        year = keyboard.nextInt();    
+        System.out.println("During what year does the schedyle start? (Ex: 2017)");
+        startY = keyboard.nextInt();    
         System.out.println();
         
     }
@@ -49,26 +49,26 @@ public class ScheduleDates
     public static void ScheduleEnd(){
         Scanner keyboard = new Scanner(System.in); 
         
-        String DoW;
-        int day;
-        int month;
-        int year;
+        String endDoW;
+        int endD;
+        int endM;
+        int endY;
         
         System.out.println("On what day of the week does the schedule end? (Ex: Friday) ");
-        DoW = keyboard.nextLine();
+        endDoW = keyboard.nextLine();
         System.out.println();
         
-        System.out.println("On which day of the month does this day fall? (Ex: Enter '14' for Monday, Dec. 14th) ");
-        day = keyboard.nextInt();    
+        System.out.println("On which day of the month does the schedule end? (Ex: Enter '14' for Monday, Dec. 14th) ");
+        endD = keyboard.nextInt();    
         System.out.println();
         
-        System.out.println("On which month does the schedule begin? ");
+        System.out.println("During what month does the schedule begin? ");
         System.out.println("Enter the month's number. (Ex: For January, enter '1' ");
-        month = keyboard.nextInt();    
+        endM = keyboard.nextInt();    
         System.out.println();
         
-        System.out.println("What year is it?");
-        year = keyboard.nextInt();    
+        System.out.println("During what year does the schedule end? (Ex: 2018)");
+        endY = keyboard.nextInt();    
         System.out.println();
         
     }
