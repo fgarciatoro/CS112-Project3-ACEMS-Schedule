05c0b407818832e0ecfbffa11922426403085c67
jgreenfield20 <33734332+jgreenfield20@users.noreply.github.com>
2017-12-05 19:47:46 -0500

    Update algorithmACEMS.java

--- algorithmACEMS.java
+++ algorithmACEMS.java
@@ -6,9 +6,13 @@ public algorithmACEMS(){
 
 }
 
+public static void assignPoints(){
+  
+}
 
-
-
+//so we call Shift(int shiftNum) with the variable shiftNum, so we need to use that s the index for the ith availability of all the members and put that into an ArrayList<ArrayList<String memberName>> so that for each shift we know which members can take it
+  
+  //Then, we will parse through that double arraylist and for each shift, the people who are amrked yes get ( 1000/numPeopleWhoCanTakeThisShift ) (becuase even if like 20 people can take the shift, they'll all still get something which relatively will make it so that the one with the most points gets it, I think...) and maybe people get like 10 points or something  
 
 
 
