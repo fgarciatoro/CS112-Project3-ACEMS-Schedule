18db6e58b4731f1d49383364ae426d8210bb7e35
whiteandnerdy42 <33734328+whiteandnerdy42@users.noreply.github.com>
2017-12-09 14:19:52 -0500

    Implemented PointedMember class

--- Member.java
+++ Member.java
@@ -4,14 +4,27 @@ public class Member {
   String rank;
   ArrayList<String> availability; // 1 means yes, -1 is no, 0 is if need be
   int numShifts;
+  double points;
   
   public Member(String thisName, String thisRank,ArrayList<String> thisAvailability) {
     name = thisName;
     rank = thisRank;
     availability = thisAvailability;
     numShifts=0;
+    points = 0.0;
     }
   public void plusShifts(){
     numShifts++;
+    return;
   }
+  public void addPoints(double toAdd){
+    points = points + toAdd;
+  return;
+}
+
+public void subtractPoints(double toSubtract){
+    points = points - toSubtract;
+  return;
+}
+
 }  
