8d43bd49cf89bf62fd95bf8106ad29fa3bfe702b
whiteandnerdy42 <33734328+whiteandnerdy42@users.noreply.github.com>
2017-12-13 13:54:01 -0500

    Update Member.java

--- Member.java
+++ Member.java
@@ -2,10 +2,11 @@ import java.util.ArrayList;
 public class Member {
   String name;
   String rank;
-  ArrayList<String> availability; // 1 means yes, -1 is no, 0 is if need be
+  ArrayList<String> availability; // 1 means yes, -1 is no, 0 is if need be; FileReader assigns these to an ArrayList
   int numShifts;
   double points;
   
+  // Constructor, initializing points to 0 and assuming each member starts with 0 shifts
   public Member(String thisName, String thisRank,ArrayList<String> thisAvailability) {
     name = thisName;
     rank = thisRank;
